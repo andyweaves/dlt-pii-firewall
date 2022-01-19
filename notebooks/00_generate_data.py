58c5768ad0c9591a1d8120ae20a262a98f3c7500
andrew.weaver@databricks.com <andrew.weaver@databricks.com>
2022-01-19 19:56:06 +0000

    Optionally generate clean data for testing

--- notebooks/00_generate_data.py
+++ notebooks/00_generate_data.py
@@ -11,6 +11,12 @@ OUTPUT_DIR = dbutils.widgets.get("OUTPUT_DIR")
 
 # COMMAND ----------
 
+dbutils.widgets.dropdown("GENERATE_CLEAN_DATA", defaultValue="False", choices=["True", "False"])
+
+GENERATE_CLEAN_DATA = dbutils.widgets.get("GENERATE_CLEAN_DATA") == "True"
+
+# COMMAND ----------
+
 import pandas as pd
 from typing import Iterator
 from pyspark.sql.functions import *
@@ -24,7 +30,7 @@ schema = StructType([
   StructField("name", StringType(), False),
   StructField("email", StringType(), False),
   StructField("date_of_birth", DateType(), False),
-  StructField("age", IntegerType(), False),
+  StructField("age", LongType(), False),
   StructField("address", StringType(), False),
   StructField("ip_address", StringType(), False),
   StructField("phone_number", StringType(), False),
@@ -71,26 +77,28 @@ def generate_fake_data(pdf: pd.DataFrame) -> pd.DataFrame:
     
   return pdf.apply(generate_data, axis=1).drop(["partition_id", "id"], axis=1)
 
-pii_data = spark.range(1, NUM_ROWS + 1).withColumn("customer_id", get_customer_id(col("id"))).withColumn("partition_id", spark_partition_id()).groupBy("partition_id").applyInPandas(generate_fake_data, schema).orderBy(asc("customer_id"))
+customer_data = spark.range(1, NUM_ROWS + 1).withColumn("customer_id", get_customer_id(col("id"))).withColumn("partition_id", spark_partition_id()).groupBy("partition_id").applyInPandas(generate_fake_data, schema).orderBy(asc("customer_id"))
 
-display(pii_data)
+display(customer_data)
 
 # COMMAND ----------
 
 import pandas as pd
- 
-# Generate some "clean" data which doesn't contain PII and union them to our data which contains generated PII...
-data = [
-  [10001, "John", "", date.today(), 1965, "Strawberry Fields", "999.999.999.999", "1234", "Taxman", "", 1234, "Yesterday", "1", "There are places I'll remember all my life though some have changed"], 
-  [20001, "Paul", "", date.today(), 1966, "Penny Lane", "1.1.1.01", "5678", "Money (That's What I Want)", "", 5678, "Eight Days a Week", "2", "And in the end the love you take is equal to the love you make"], 
-  [30001, "Ringo", "", date.today(), 1967, "Abbey Road", "255.255.255.256", "Parlophone", "You Never Give Me Your Money", "", 1111, "Tomorrow Never Knows", "3", "How does it feel to be one of the beautiful people?"], 
-  [40001, "George", "", date.today(), 1969, "Octopus's Garden", "0.1", "No Reply", "Back in the U.S.S.R.", "", 2222, "Timeless", "4", "Something in the way she moves..."]]
- 
-pdf = pd.DataFrame(data, columns = ["customer_id", "name", "email", "date_of_birth", "age", "address", "ip_address", "phone_number", "ssn", "iban", "credit_card", "expiry_date", "security_code", "freetext"])
 
-clean_data = spark.createDataFrame(pdf) 
+if GENERATE_CLEAN_DATA:
+ 
+  # Generate some "clean" data which doesn't contain PII and union them to our data which contains generated PII...
+  data = [
+    [10001, "John", "", date.today(), 1, "Strawberry Fields", "999.999.999.999", "1234", "Taxman", "", 1234, "Yesterday", "1", "There are places I'll remember all my life though some have changed"], 
+    [20001, "Paul", "", date.today(), 9, "Penny Lane", "1.1.1.01", "5678", "Money (That's What I Want)", "", 5678, "Eight Days a Week", "2", "And in the end the love you take is equal to the love you make"], 
+    [30001, "Ringo", "", date.today(), 6, "Abbey Road", "255.255.255.256", "Parlophone", "You Never Give Me Your Money", "", 1111, "Tomorrow Never Knows", "3", "How does it feel to be one of the beautiful people?"], 
+    [40001, "George", "", date.today(), 8, "Octopus's Garden", "0.1", "No Reply", "Back in the U.S.S.R.", "", 2222, "Timeless", "4", "Something in the way she moves..."]]
 
-customer_data = pii_data.union(clean_data)
+  pdf = pd.DataFrame(data, columns = ["customer_id", "name", "email", "date_of_birth", "age", "address", "ip_address", "phone_number", "ssn", "iban", "credit_card", "expiry_date", "security_code", "freetext"])
+
+  clean_data = spark.createDataFrame(pdf) 
+
+  customer_data = customer_data.union(clean_data)
 
 # COMMAND ----------
 
