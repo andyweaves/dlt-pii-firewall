8017d0ebf89c63d7cc1dbfd50df9f736aa760ffd
andrew.weaver@databricks.com <andrew.weaver@databricks.com>
2022-06-09 19:52:50 +0000

    Make FILL_NULLS optional

--- notebooks/02_detect_and_redact_pii.py
+++ notebooks/02_detect_and_redact_pii.py
@@ -4,22 +4,15 @@ INPUT_FORMAT = spark.conf.get("INPUT_FORMAT")
 TABLE_PATH = spark.conf.get("TABLE_PATH")
 EXPECTATIONS_PATH = spark.conf.get("EXPECTATIONS_PATH")
 NUM_SAMPLE_ROWS = int(spark.conf.get("NUM_SAMPLE_ROWS"))
-FILL_NULLS = eval(spark.conf.get("FILL_NULLS"))
 
 # COMMAND ----------
 
-def get_spark_read(input_format, input_path, fill_nulls):
+def get_spark_read(input_format, input_path):
   
   if input_format == "csv":
-    if fill_nulls:
-      return spark.read.format(input_format).load(input_path, header=True, inferSchema=True).na.fill("")
-    else:
-      return spark.read.format(input_format).load(input_path, header=True, inferSchema=True)
+    return spark.read.format(input_format).load(input_path, header=True, inferSchema=True)
   else: 
-    if fill_nulls:
-      return spark.read.format(input_format).load(input_path).na.fill("")
-    else: 
-      return spark.read.format(input_format).load(input_path)
+    return spark.read.format(input_format).load(input_path)
 
 # COMMAND ----------
 
@@ -55,7 +48,7 @@ def get_expectations_and_actions(schema, expectations_path):
 
 # COMMAND ----------
 
-schema = get_spark_read(INPUT_FORMAT, INPUT_PATH, FILL_NULLS).schema
+schema = get_spark_read(INPUT_FORMAT, INPUT_PATH).schema
 expectations_and_actions, nested_columns = get_expectations_and_actions(schema, EXPECTATIONS_PATH)
 
 # COMMAND ----------
@@ -75,35 +68,35 @@ import pyspark.sql.functions as F
 constraints = dict(zip(expectations_and_actions.expectation, expectations_and_actions.constraint))
 
 def get_sql_expressions(schema, nested_columns):
-  
-  all_columns = schema.fieldNames()
-  not_nested = [col for col in all_columns if col not in nested_columns]
-  select_sql = not_nested + list({f"{col}.*" for col in nested_columns})
-
-  df = get_spark_read(INPUT_FORMAT, INPUT_PATH, FILL_NULLS).limit(NUM_SAMPLE_ROWS).selectExpr(select_sql)
-  
-  new_columns = df.columns
-  
-  # Drop duplicates because otherwise we'll need to handle duplicate columns in the downstream tables, which will get messy...
-  pdf = df.withColumn("failed_expectations", F.array([F.expr(value) for key, value in constraints.items()])).withColumn("failed_expectations", get_failed_expectations("failed_expectations")).filter(F.size("failed_expectations") > 0).select(explode("failed_expectations").alias("expectation")).distinct().withColumn("failed_column", regexp_extract(col("expectation"), "\`(.*?)\`", 1)).toPandas().drop_duplicates(subset = ["failed_column"]).merge(expectations_and_actions, on="expectation")
-  
-  pii_detected = False
-  
-  if len(pdf) > 0:
-    pii_detected = True
-  
-  # Todo - change to list comprehension. It's more performant...
-  redact_sql = [col for col in new_columns if col not in pdf["failed_column"].tolist()]
-  
-  def generate_sql(row):
-    if row["mode"] in ["REDACT", "REDACT_AND_TAG"]:
-      redact_sql.append(row["action"])
-    elif row["mode"] == "TAG":
-      redact_sql.append(row["failed_column"])  
-
-  pdf.apply(generate_sql, axis=1)
     
-  return select_sql, redact_sql, pii_detected
+    all_columns = schema.fieldNames()
+    not_nested = [col for col in all_columns if col not in nested_columns]
+    select_sql = not_nested + list({f"{col}.*" for col in nested_columns})
+
+    df = get_spark_read(INPUT_FORMAT, INPUT_PATH).limit(NUM_SAMPLE_ROWS).selectExpr(select_sql).na.fill("")
+
+    new_columns = df.columns
+
+    # Drop duplicates because otherwise we'll need to handle duplicate columns in the downstream tables, which will get messy...
+    pdf = df.withColumn("failed_expectations", F.array([F.expr(value) for key, value in constraints.items()])).withColumn("failed_expectations", get_failed_expectations("failed_expectations")).filter(F.size("failed_expectations") > 0).select(explode("failed_expectations").alias("expectation")).distinct().withColumn("failed_column", regexp_extract(col("expectation"), "\`(.*?)\`", 1)).toPandas().drop_duplicates(subset = ["failed_column"]).merge(expectations_and_actions, on="expectation")
+    
+    pii_detected = False
+
+    if len(pdf) > 0:
+        pii_detected = True
+
+    # Todo - change to list comprehension. It's more performant...
+    redact_sql = [col for col in new_columns if col not in pdf["failed_column"].tolist()]
+
+    def generate_sql(row):
+        if row["mode"] in ["REDACT", "REDACT_AND_TAG"]:
+            redact_sql.append(row["action"])
+        elif row["mode"] == "TAG":
+            redact_sql.append(row["failed_column"])  
+
+    pdf.apply(generate_sql, axis=1)
+
+    return select_sql, redact_sql, pii_detected
 
 # COMMAND ----------
 
@@ -120,7 +113,7 @@ import dlt
 def staging():
   
     return (
-      get_spark_read(INPUT_FORMAT, INPUT_PATH, FILL_NULLS).selectExpr(select_sql)
+      get_spark_read(INPUT_FORMAT, INPUT_PATH).selectExpr(select_sql).na.fill("")
   )
 
 # COMMAND ----------
